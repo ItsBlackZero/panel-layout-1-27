dc9ec49a67031287dbf806565aaf3310ebe040dd
unknown <yoangel1221@gmail.com>
2022-08-20 12:35:23 -0500

    add new change

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("New Change");
 		new FrmPrincipal().setVisible(true);
 	}
 
